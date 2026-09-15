23ce19bce843ab366c93e2f177bbc8fcea001a4f
abevitor <blueprates86@gmail.com>
2026-09-15 16:31:40 -0300

    feat: implementar lógica de animação para o componente TerminalText

--- src/components/TerminalText/TerminalText.tsx
+++ src/components/TerminalText/TerminalText.tsx
@@ -14,4 +14,60 @@ export function TerminalText({
     const [visibleLines, setVisibleLines] = useState<string[]>([])
     const [currentLine, setCurrentLine] = useState(0)
     const [currentCharacter, setCurrentCharacter] = useState(0)
+
+    useEffect(() => {
+        const startTimeout = window.setTimeout(() => {
+            setVisibleLines([])
+            setCurrentLine(0)
+            setCurrentCharacter(0)
+        }, delay)
+
+        return () => window.clearTimeout(startTimeout)
+
+    }, [delay])
+
+    useEffect(() => {
+        if(currentLine >= lines.length){
+            return
+        }
+
+        const line = lines[currentLine]
+
+        if(currentCharacter < line.length) {
+            const timeout = window.setTimeout(() => {
+                setVisibleLines((previous) => {
+                    const updated = [...previous]
+                    updated[currentLine] = line.slice(0, currentCharacter + 1)
+                return updated
+                            })
+
+                            setCurrentCharacter((previous) => previous + 1)
+            }, speed)
+            
+            return () => window.clearTimeout(timeout)
+        }
+
+        const timeout = window.setTimeout(() => {
+            setCurrentLine((previous) => previous + 1)
+            setCurrentCharacter(0)
+        }, 180)
+
+        return () => window.clearTimeout(timeout)
+
+    }, [ currentCharacter, currentLine, lines, speed])
+
+     return (
+    <div className="terminal-text">
+      {visibleLines.map((line, index) => (
+        <div className="terminal-line" key={`${line}-${index}`}>
+          <span className="terminal-prompt">&gt;</span>
+          <span>{line}</span>
+
+          {index === currentLine && currentLine < lines.length && (
+            <span className="terminal-cursor">_</span>
+          )}
+        </div>
+      ))}
+    </div>
+  )
 }
